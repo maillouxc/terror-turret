08b7dfc6b4ca8d8eb7de5b1505501a998c57659f
Chris Mailloux <mailloux.cl@gmail.com>
2018-08-26 12:27:14 -0400

    Initial project configuration

---
+++ android/app/src/androidTest/java/edu/fgcu/terrorturret/ExampleInstrumentedTest.kt
@@ -0,0 +1,26 @@
+package edu.fgcu.terrorturret
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+
+    @Test
+    fun test_universe_not_broken() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("edu.fgcu.terrorturret", appContext.packageName)
+    }
+
+}
