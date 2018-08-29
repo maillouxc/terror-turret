fb05d7c03461599aae998401ae511b2497a581f0
Chris Mailloux <mailloux.cl@gmail.com>
2018-08-29 18:03:21 -0400

    Add basic control interface

---
+++ android/app/src/main/java/edu/fgcu/terrorturret/LoggerTags.kt
@@ -0,0 +1,11 @@
+package edu.fgcu.terrorturret
+
+/**
+ * This object contains variables defining "tag" strings that can be used to categorize logging
+ * statements for our logger.
+ */
+object LoggerTags {
+
+    const val LOG_TURRET_CONTROL = "TURRET_CONTROL"
+
+}
