6b4fb4f7fae2dc6de57213f202c22f4bd18abb04
Chris Mailloux <mailloux.cl@gmail.com>
2018-09-01 15:58:43 -0400

    Fire control system feature complete - untested
    This implementation has not been checked for correctness, and it does
    not feature the necessary code to interact with a Raspberry Pi. This
    will be added soon.

--- geekduino/fire_control_system/fire_control_system.ino
+++ geekduino/fire_control_system/fire_control_system.ino
@@ -1,71 +1,99 @@
 /************************************************************************************
- * Fire control system for the Terror Turret.
+   Fire control system for the Terror Turret.
  ************************************************************************************
- * Wiring is as follows:
- * 
- * Laser - Digital Pin 2
- * Trigger - Digital Pin 3
- * Pushbutton - Digital Pin 4
- * Pan Servo - Digital Pin 10
- * Tilt Servo - Digital Pin 11
- * 
- * Joystick (Horizontal) - Analog Pin 0
- * Joystick (Vertical) - Analog Pin 2
- * 
- * We are currently not using the joystick to do anything, but it will remain
- * wired up for now in case we need it for something, and so the pins and other
- * relevant variables will remain defined in the code for potential usage later.
+   Wiring is as follows:
+
+   Laser - Digital Pin 2
+   Pushbutton - Digital Pin 4
+   Trigger - Digital Pin 7
+   Pan Servo - Digital Pin 10
+   Tilt Servo - Digital Pin 11
+
+   Joystick (Horizontal) - Analog Pin 0
+   Joystick (Vertical) - Analog Pin 2
+
+   The joystick and button are only here for testing purposes at the moment.
+   This will eventually take its instructions from a Raspberry Pi.
  ***********************************************************************************/
 
 #include <Servo.h>
 
 /************************************************************************************
- * Constants
+   Constants
  ***********************************************************************************/
 // Digital pin assignments
+const int LASER_PIN = 2;
+const int PUSHBUTTON_PIN = 4;
+const int TRIGGER_PIN = 7;
 const int PAN_SERVO_PIN = 10;
 const int TILT_SERVO_PIN = 11;
-const int LASER_PIN = 2;
-const int PUSHBUTTON_PIN = 2;
 
 // Analog pin assignments
 const int HORIZONTAL_JOY_PIN = 0;
 const int VERTICAL_JOY_PIN = 1;
 
-// Calibration related constants
-const int JOY_DEADZONE_LOW = 480;
-const int JOY_DEADZONE_HIGH = 540;
+// Input handling related adjustable constants
+const int JOY_DEADZONE_X_LOW = 480;
+const int JOY_DEADZONE_X_HIGH = 540;
+const int JOY_DEADZONE_Y_LOW = 480;
+const int JOY_DEADZONE_Y_HIGH = 540;
+const int DEBOUNCE_DELAY_MS = 50;
+
+// Needed to compensate for servo centered position being slightly wrong
+const int TURRET_PITCH_CALIBRATION_MICROSECONDS = 100;
+
+const int JOY_MIN_VALUE = 0;
+const int JOY_MAX_VALUE = 1023;
+
+// These constants represent the microsecond pulse values defining the range of
+// motion for the type of servos we are using - the servos we use have a 180 degree
+// range, so the min value here represents 0 degrees and the max represents 180.
+// Note that the actual allowed range of motion is limited by our code to prevent
+// the gun from hitting the turret itself, plus a few other reasons.
+const int SERVO_MIN_MICROSECS = 600;
+const int SERVO_MAX_MICROSECS = 2400;
+const int SERVO_MIN_DEGREES = 0;
+const int SERVO_MAX_DEGREES = 180;
 
 // Turret movement constraint settings
-// Note that 90 degrees is "centered" on these servos
+// These determine where the turret is ALLOWED to move, NOT where it CAN move.
+// Note that 90 degrees is centered
 const int PAN_MIN_DEGREES = 0;
 const int PAN_MAX_DEGREES = 180;
 const int TILT_MIN_DEGREES = 45;
 const int TILT_MAX_DEGREES = 135;
 
-// Needed to compensate for servo centered position being slightly wrong
-const int TURRET_PITCH_CALIBRATION_DEGREES = 20;
+const int INITIAL_PAN_VALUE_MICROSECS = 1500; // Centered
+const int INITIAL_TILT_VALUE_MICROSECS = 1500; // Centered
+
+const int MAX_SERVO_SPEED = 10; // Should be between 5 and 500
 /***********************************************************************************/
+
+int degreesToMicrosecs(unsigned int);
 
 // Servo objects used to control the servos
 Servo panServo;
 Servo tiltServo;
 
+int panMinMicrosecs = degreesToMicrosecs(PAN_MIN_DEGREES);
+int panMaxMicrosecs = degreesToMicrosecs(PAN_MAX_DEGREES);
+int tiltMinMicrosecs = degreesToMicrosecs(TILT_MIN_DEGREES);
+int tiltMaxMicrosecs = degreesToMicrosecs(TILT_MAX_DEGREES);
+
 // Current state variables for the turret
-int panValue = 90;
-int tiltValue = 90;
+int panValueMicrosecs = INITIAL_PAN_VALUE_MICROSECS;
+int tiltValueMicrosecs = INITIAL_TILT_VALUE_MICROSECS;
+
 bool isSafetyOn = true;
 int laserState = LOW;
 int buttonState = LOW;
 int lastButtonState = LOW;
-
 long lastDebounceTime = 0;
-int debounceDelay = 50;
 
 /**
- * Code in this method will run when the board is first powered.
- */
-void setup() 
+   Code in this method will run when the board is first powered.
+*/
+void setup()
 {
   setupPins();
   engageSafety();
@@ -73,63 +101,82 @@ void setup()
 }
 
 /**
- * Code in this method will run repeatedly.
- */
-void loop() 
+   Code in this method will run repeatedly.
+*/
+void loop()
 {
-  readAndDebounceButton();
-  ensureServoPositionsAreWithinAllowedRange();
-  panServo.write(panValue);
-  panServo.write(tiltValue);
+  startOrStopFiringBasedOnButtonState();
+  readAnalogSticksAndSetDesiredServoPositions();
+  ensureDesiredServoPositionsAreInAllowedRange();
+  panServo.writeMicroseconds(panValueMicrosecs);
+  panServo.writeMicroseconds(tiltValueMicrosecs);
+  delay(15);
 }
 
-void setupPins() 
+void setupPins()
 {
-  panServo.attach(PAN_SERVO_PIN);
-  tiltServo.attach(TILT_SERVO_PIN);
+  panServo.attach(PAN_SERVO_PIN, SERVO_MIN_MICROSECS, SERVO_MAX_MICROSECS);
+  tiltServo.attach(TILT_SERVO_PIN, SERVO_MIN_MICROSECS, SERVO_MAX_MICROSECS);
+
   pinMode(PUSHBUTTON_PIN, INPUT);
   pinMode(LASER_PIN, OUTPUT);
 }
 
-void moveServosToInitialPosition() 
+void moveServosToInitialPosition()
 {
-  panServo.write(panValue);
+  panServo.write(panValueMicrosecs);
   delay(1000);
-  tiltServo.write(tiltValue);
+  tiltServo.write(tiltValueMicrosecs);
   delay(1000);
 }
 
-void engageSafety() 
+void engageSafety()
 {
   isSafetyOn = true;
   setLaserOn(false);
 }
 
-void disengageSafety() 
+void disengageSafety()
 {
   isSafetyOn = false;
   setLaserOn(true);
 }
 
-void setLaserOn(bool laserOn) 
+void setLaserOn(bool laserOn)
 {
-  if (laserOn) {
-    digitalWrite(LASER_PIN, HIGH);
-  } 
-  else {
-    digitalWrite(LASER_PIN, LOW);
+  int pinValue = laserOn ? HIGH : LOW;
+  digitalWrite(LASER_PIN, pinValue);
+}
+
+void readAnalogSticksAndSetDesiredServoPositions()
+{
+  // Analog stick values range between 0 and 1023
+  int horizontalJoyValue = analogRead(HORIZONTAL_JOY_PIN);
+  int verticalJoyValue = analogRead(VERTICAL_JOY_PIN);
+  int speed = MAX_SERVO_SPEED;
+
+  // Handle pan
+  if (horizontalJoyValue < JOY_DEADZONE_X_LOW
+      || horizontalJoyValue > JOY_DEADZONE_X_HIGH) {
+    panValueMicrosecs += (1.0 * map(horizontalJoyValue, 0, 1023, -speed, speed));
+  }
+
+  // Handle tilt
+  if (verticalJoyValue < JOY_DEADZONE_Y_LOW
+      || verticalJoyValue > JOY_DEADZONE_Y_HIGH) {
+    tiltValueMicrosecs += (1.0 * map(verticalJoyValue, 0, 1023, -speed, speed));
   }
 }
 
-void ensureServoPositionsAreWithinAllowedRange() 
+void ensureDesiredServoPositionsAreInAllowedRange()
 {
-  panValue = max(panValue, PAN_MIN_DEGREES);
-  panValue = min(panValue, PAN_MAX_DEGREES);
-  tiltValue = max(tiltValue, TILT_MIN_DEGREES);
-  tiltValue = max(tiltValue, TILT_MAX_DEGREES);
+  panValueMicrosecs = max(panValueMicrosecs, panMinMicrosecs);
+  panValueMicrosecs = min(panValueMicrosecs, panMaxMicrosecs);
+  tiltValueMicrosecs = max(tiltValueMicrosecs, tiltMinMicrosecs);
+  tiltValueMicrosecs = min(tiltValueMicrosecs, tiltMaxMicrosecs);
 }
 
-void readAndDebounceButton() 
+int readAndDebounceButton()
 {
   int reading = digitalRead(PUSHBUTTON_PIN);
   if (reading != lastButtonState) {
@@ -137,19 +184,37 @@ void readAndDebounceButton()
   }
 
   long timeSinceStateChange = millis() - lastDebounceTime;
-  if (timeSinceStateChange > debounceDelay) {
+  if (timeSinceStateChange > DEBOUNCE_DELAY_MS) {
     if (reading != buttonState) {
       // Button is now debounced, so reading should be the correct state
       buttonState = reading;
     }
   }
   lastButtonState = reading;
+  return buttonState;
 }
 
-void fire() 
+void startOrStopFiringBasedOnButtonState()
 {
-  if (isSafetyOn) {
-    return;
+  int reading = readAndDebounceButton();
+  if (reading == HIGH) {
+    // Begin firing
+    if (!isSafetyOn) {
+      digitalWrite(TRIGGER_PIN, HIGH);
+    }
+  } else {
+    // Stop firing
+    digitalWrite(TRIGGER_PIN, LOW);
   }
-  /* TODO */
+}
+
+int degreesToMicrosecs(unsigned int angleDegrees)
+{
+  float microsecs = map(angleDegrees,
+                        SERVO_MIN_MICROSECS,
+                        SERVO_MAX_MICROSECS,
+                        SERVO_MIN_DEGREES,
+                        SERVO_MAX_DEGREES);
+
+  return round(microsecs);
 }
