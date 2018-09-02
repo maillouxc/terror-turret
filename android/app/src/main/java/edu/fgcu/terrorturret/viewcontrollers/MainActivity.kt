59d258e033bfe1b8d962035f16c0a0d43f8e2940
Chris Mailloux <mailloux.cl@gmail.com>
2018-09-02 12:32:07 -0400

    Add login screen to turret app

--- android/app/src/main/java/edu/fgcu/terrorturret/viewcontrollers/MainActivity.kt
+++ android/app/src/main/java/edu/fgcu/terrorturret/viewcontrollers/MainActivity.kt
@@ -3,12 +3,26 @@ package edu.fgcu.terrorturret.viewcontrollers
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import edu.fgcu.terrorturret.R
+import edu.fgcu.terrorturret.applogic.RobotController
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        registerClickListeners()
+    }
+
+    private fun registerClickListeners() {
+        // TODO
+    }
+
+    private fun onClickFireButton() {
+        RobotController.fireZeMissiles()
+    }
+
+    private fun onAnalogStickMoved(xPos: Int, yPos: Int) {
+        // TODO
     }
 
 }
