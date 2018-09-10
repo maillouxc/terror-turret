73205a415e5ca5f834a47b861de19c5b81af0728
Christopher Mailloux <mailloux.cl@gmail.com>
2018-09-10 19:13:36 -0400

    Fix spacing

--- pi/rpi_setup.sh
+++ pi/rpi_setup.sh
@@ -61,7 +61,6 @@ echo "Installing python-serial..."
 sudo apt-get install python-serial || exiterror
 echo "Installing pyfirmata..."
 sudo pip install pyfirmata || exiterror
-
 echo "Finished installing needed packages\n"
 
 success
