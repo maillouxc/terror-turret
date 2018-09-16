2f3d351e2d2344e2f3c44079f585776c8475adf9
Chris Mailloux <mailloux.cl@gmail.com>
2018-09-16 12:58:25 -0400

    Write (untested) test function for turret commands

--- pi/rpi-turret-manager.py
+++ pi/rpi-turret-manager.py
@@ -3,6 +3,7 @@
 # This Python program is the main Rpi control program for the turret
 
 import serial
+from time import sleep
 
 ARDUINO_SERIAL_CONN_PORT = '/dev/tty/USB0'
 SERIAL_BAUD_RATE = 9600
@@ -23,13 +24,63 @@ arduinoSerialConn = serial.Serial(ARDUINO_SERIAL_CONN_PORT, SERIAL_BAUD_RATE)
 
 
 def main():
+    print("Turret manager software started.")
     testTurretCommands()
 
-def sendCommandToTurret(command):
-    serial.write(command)
+
+def commandTurret(command):
+    serial.write(str(command))
     
+
 def testTurretCommands():
-    print("TODO")
+    print("Initiating turret commands test...")
+    sleep(1)
+
+    print("Commanding SAFETY OFF")
+    commandTurret(CMD_SAFETY_OFF)
+    sleep(1)
+
+    print("Commanding SAFETY ON")
+    commandTurret(CMD_SAFETY_ON)
+    sleep(1)
+
+    print("Commanding SAFETY OFF")
+    commandTurret(CMD_SAFETY_OFF)
+    sleep(1)
+
+    print("Firing for 1 second")
+    commandTurret(CMD_FIRE)
+    sleep(1)
+    commandTurret(CMD_STOP_FIRE)
+    sleep(1)
+
+    print("Left at speed 7")
+    commandTurret(CMD_ROTATE_ZERO - 7)
+    sleep(2)
+
+    print("Right at speed 3")
+    commandTurret(CMD_ROTATE_ZERO + 3)
+    sleep(3)
+
+    print("Up at speed 10")
+    commandTurret(CMD_PITCH_UP_MAX)
+    sleep(2)
+
+    print("Down at speed 1")
+    commandTurret(CMD_PITCH_ZERO - 1)
+    sleep(5)
+
+    print("Testing moving and firing")
+    commandTurret(CMD_ROTATE_ZERO + 3)
+    commandTurret(CMD_FIRE)
+    sleep(1)
+    commandTurret(CMD_STOP_FIRE)
+    sleep(2)
+
+    print("Turning safety back on")
+    comandTurret(CMD_SAFETY_ON)
+
+    print("Test complete.")
 
 
 if __name__ == "__main__":
