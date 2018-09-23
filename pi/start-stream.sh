a2c595a49126962ccfd84ce15dec6d8ac55f2987
Chris Mailloux <mailloux.cl@gmail.com>
2018-09-22 21:13:56 -0400

    Try to fix killing UV4L during startup

--- pi/start-stream.sh
+++ pi/start-stream.sh
@@ -3,7 +3,7 @@
 # This file contains a simple script to start the UV4L video stream
 
 echo "Killing any existing UV4L stream stuff that may get in the way..."
-sudo /usr/bin/pkill || echo "You must this script as root!"
+sudo /usr/bin/pkill uv4l || echo "\n You must run this script as root!\n"
 
 echo "Starting UV4L server on port 9000..."
 uv4l --driver uvc \
