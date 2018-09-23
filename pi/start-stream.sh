e954f98a7a1b0190ae0a9043d87571340d70fe46
Christopher Mailloux <mailloux.cl@gmail.com>
2018-09-23 14:36:48 -0400

    Add config file argument to startup script

--- pi/start-stream.sh
+++ pi/start-stream.sh
@@ -5,17 +5,17 @@
 echo "Killing any existing UV4L stream stuff that may get in the way..."
 sudo /usr/bin/pkill uv4l || echo "You must run this script as root!"
 
-# Give some time so that hopefully the process is dead
+# Give some time so that hopefully the process is dead before continuing
 sleep 3
 
 echo "Starting UV4L server on port 8080..."
 uv4l --driver uvc \
-    --verbosity 8 \
 	--device-id 046d:0825 \
 	--frame-buffers 2 \
-	--auto-video_nr
+	--auto-video_nr \
+	--config-file "./uv4l-config.conf"
 	
-echo "Turret stream *should* be up and running."
+echo -e "\nTurret stream *should* be up and running!\n"
 
-echo "If the device name isn't /dev/video0, it didn't work."
-echo "It can sometimes help to run the script again in that case."
+echo "If the device name just printed above isn't /dev/video0, it didn't work."
+echo "It can sometimes help to run this script again in that case."
