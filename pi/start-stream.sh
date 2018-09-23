979294664d013f4f3e2616647251e32059a830cf
Chris Mailloux <mailloux.cl@gmail.com>
2018-09-22 21:17:55 -0400

    Try to fix killing UV4L during startup

--- pi/start-stream.sh
+++ pi/start-stream.sh
@@ -5,6 +5,9 @@
 echo "Killing any existing UV4L stream stuff that may get in the way..."
 sudo /usr/bin/pkill uv4l || echo "\n You must run this script as root!\n"
 
+# Give some time so that hopefully the process is dead
+sleep 3
+
 echo "Starting UV4L server on port 9000..."
 uv4l --driver uvc \
     --verbosity 8 \
@@ -12,4 +15,7 @@ uv4l --driver uvc \
 	--frame-buffers 2 \
 	--auto-video_nr
 	
-echo "Turret stream *should* be up and running."
+echo "\n\nTurret stream *should* be up and running.\n"
+
+echo "If the device name isn't /dev/video0, it didn't work."
+echo "It can sometimes help to run the script again in that case."
