e84de3a66b0508b6a97a9fe54ad3bc8b5ef9e9d6
Christopher Mailloux <mailloux.cl@gmail.com>
2018-09-23 12:35:42 -0400

    Fix broken sources add

--- pi/rpi_setup.sh
+++ pi/rpi_setup.sh
@@ -73,7 +73,7 @@ sudo pip install git+https://github.com/dpallot/simple-websocket-server.git || e
 
 echo -e "\nInstalling UV4L..."
 curl http://www.linux-projects.org/listing/uv4l_repo/lpkey.asc | sudo apt-key add -
-echo "deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/stretch stretch main" >> /etc/apt/sources.list
+echo "deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/stretch stretch main" | sudo tee -a /etc/apt/sources.list
 sudo apt-get install uv4l || exiterror
 sudo apt-get install uv4l-raspicam || exiterror
 sudo apt-get install uv4l-raspicam-extras || exiterror
