334b69b3de958b233c84c22bb001b9f410d48d25
Chris Mailloux <mailloux.cl@gmail.com>
2018-09-22 21:04:48 -0400

    Update start-stream.sh to the new working settings

--- pi/start-stream.sh
+++ pi/start-stream.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-# This file contains a simple script to start the raspicam stream
-# For whatever reason, we've screwed something up and it doesn't start
-# by default anymore, and we can't figure out how to fix it.
-# So instead, just run this script whenever you need to start the stream.
+# This file contains a simple script to start the UV4L video stream
+
+echo "Killing any existing UV4L stream stuff that may get in the way..."
+pkill uv4l || echo "You must this script as root!"
 
 echo "Starting UV4L server on port 9000..."
-
-uv4l --config-file='uv4l-config.conf' \
-	--driver-config-file='uv4l-config.conf' \
-	--external-driver \
-	--device-name=video0
+uv4l --driver uvc \
+    --verbosity 8 \
+	--device-id 046d:0825 \
+	--frame-buffers 2 \
+	--auto-video_nr
+	
+echo "Turret stream *should* be up and running."
