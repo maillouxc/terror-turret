7eedaf34e7a7e7d30e1294df94098898413ecc38
Chris Mailloux <mailloux.cl@gmail.com>
2018-09-22 21:19:28 -0400

    Remove newlines from startup script

--- pi/start-stream.sh
+++ pi/start-stream.sh
@@ -3,7 +3,7 @@
 # This file contains a simple script to start the UV4L video stream
 
 echo "Killing any existing UV4L stream stuff that may get in the way..."
-sudo /usr/bin/pkill uv4l || echo "\n You must run this script as root!\n"
+sudo /usr/bin/pkill uv4l || echo "You must run this script as root!"
 
 # Give some time so that hopefully the process is dead
 sleep 3
@@ -15,7 +15,7 @@ uv4l --driver uvc \
 	--frame-buffers 2 \
 	--auto-video_nr
 	
-echo "\n\nTurret stream *should* be up and running.\n"
+echo "Turret stream *should* be up and running."
 
 echo "If the device name isn't /dev/video0, it didn't work."
 echo "It can sometimes help to run the script again in that case."
